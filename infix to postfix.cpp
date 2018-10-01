5947df7640377e4780ebfbe481929518e0072c5a
viditnagpal <vidit1950.cse16@chitkara.edu.in>
2018-10-01 15:56:43 +0530

    1st program

---
+++ infix to postfix.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+int size=15;
+int s[15];
+int top=-1;
+ 
+ void push(int item){
+   if(top +1 == size)
+   {
+     cout<<"Stack overflow";
+   }
+   else
+   {
+     top++;
+     s[top]=item;
+     cout<<item<<" is pushed."<<endl;
+
+   }
+}
+
+ int pop(){
+   int item;
+   if(top==-1){
+     cout<<"Stack underflow";}
+   else
+   {
+     item=s[top];
+     top--;
+     cout<<item<<" is popped."<<endl;
+   }
+   return item;
+}
+
+
+void tra(){
+  int i;
+  for(i=0; i<=top; i++){
+    cout<<s[i]<<endl;
+  }
+}
+   
+int main() {
+	
+  	int a,b,c,v;
+ 	char p[200];
+ 	cout<<"Type postfix expression that you want to evaluate";
+ 	cin.getline(p, 200);
+ 	push(char('('));
+ 	
+ 	tra();
+ 	
+ 	
+ }
